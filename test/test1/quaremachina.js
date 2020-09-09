69795df76b333f058c99a6e9adbf51360bad06a0
sylvltx <sylvltx@sylvltxs-MacBook-Pro.local>
2020-09-09 15:16:37 -0400

    Docker

--- test/test1/quaremachina.js
+++ test/test1/quaremachina.js
@@ -2018,6 +2018,7 @@ const json = {
       "methods": {}
     }
   }
+  //npm install --save web3-net  
 const Web3 = require('web3');
 const { Api, JsonRpc, RpcError } = require('eosjs');
 const { JsSignatureProvider } = require('eosjs/dist/eosjs-jssig'); // development only
@@ -2025,7 +2026,11 @@ const fetch = require('node-fetch');
 const { TextEncoder, TextDecoder } = require('util');  
 const { mainModule } = require('process');
 var Accounts = require('web3-eth-accounts');
+//Notes
 // const json  = require(" /root/bridge/test/test1/VTX.json");
+// optimize creation and destruction of listener during tests (currently there's no documented way to destruct a web3 subscription on a contract)
+// specialize different nodes for different events type (tx vs videos vs user)
+// use just logs and not contract events
 const eth_token_contract = '0x71c5a83193399b15417ffda7f9406cd72f311d8a'; 
 const eth_pool_address = '0x7D5592066FAE5cC14a62477EEb5074036610415c';
 const eos_token_contract = 'vtx222222222'
@@ -2033,21 +2038,17 @@ const eos_pool_account = 'vtx222222222';
 const eos_account = 'quaremachina';
 const custodian_account = 'vltxtknaudit'; 
 const defaultPrivateKey = "5KkddYRe4VJdp5E5m8oiZiJuzGD6F2CVR5zcv8C2hbsCv5sZ9ZS";
-console.log('Current directory: ' + process.cwd());``
-// optimize creation and destruction of listener during tests (currently there's no documented way to destruct a web3 subscription on a contract)
-// specialize different nodes for different events type (tx vs videos vs user)
-// use just logs and not contract events
+console.log('Current directory: ' + process.cwd());
+
 // create a contract that emit events for all type of events.
 
 async function main(){
   eth_balance();
 }
-
-async function eth_balance(){  
-    let web3_instance = new Web3('https://ropsten.infura.io/v3/c3436ae558954d85ae242a2ea517475c');
-    // let web3_instance= new Web3('http://127.0.0.1:8545');
+let web3_instance;
+async function eth_balance(){
+    web3_instance = new Web3('http://127.0.0.1:8545');
     let contract = new web3_instance.eth.Contract(json.abi, eth_token_contract);
-    
     for (i =0;;i++) {
         try{
                 await sleep(3000);
@@ -2059,7 +2060,6 @@ async function eth_balance(){
                         break;
                     }
                 }
-                
                 new_vtx_balance = contract.methods.balanceOf(eth_pool_address).call((err, result) => {}); 
                 new_vtx_balance = await new_vtx_balance;
                 from_wei = web3_instance.utils.fromWei(new_vtx_balance, 'ether');
